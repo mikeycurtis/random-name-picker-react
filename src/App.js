90309061e3cad91d6c6b0abfe7515571eeb9a3ff
Mikey Curtis <mjcurtis@umail.ucsb.edu>
2021-08-29 11:38:36 -0700

    Add names works. Working on deleting names

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
 function App() {
+
+  let d = new Date();
+
+  const [input,setInput] = React.useState('');
+  const [names,setNames] = React.useState([]);
+
+  const handleChange = event => {
+    return(setInput(event.target.value));
+  }
+
+  const handleRemoveName = item => {
+    const index = names.indexOf(item);
+    const newNames = names.splice(index,1);
+    
+    setNames(newNames);
+  };
+
+  const handleAddName = (item) => {
+    const newNames = names.concat({id: d.getTime(), name: item});
+    setNames(newNames);
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          poot <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Random Name Picker</h1>
+
+      <label htmlFor="textBox">Input Name: </label>
+      <input id="nameBox" type="text" value={input} onChange={handleChange}/>
+      &nbsp;
+      <button type="button" onClick={ () => handleAddName(input)}>
+        Submit
+      </button>
+
+    <List
+          list={names}
+          onRemoveItem={handleRemoveName}
+    />
     </div>
-  );
+    
+  ); 
 }
+
+const List = ({ list, onRemoveItem }) =>
+  list.map(item => (
+    <Item
+      key={item.id}
+      item={item}
+      onRemoveItem={onRemoveItem}
+    />
+));
+
+const Item = ({item, onRemoveItem}) => (
+  <div>
+    <span>{item.name}</span>
+
+    <span>
+      <button type="button" onClick={() => onRemoveItem(item)}>
+        Dismiss
+      </button>
+    </span>
+  </div>
+  
+)
+
 
 export default App;
